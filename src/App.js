7cc74c8ea82963307f2d9e1d05797a41a39cd905
Patrick <patrick204nqh@gmail.com>
2020-05-08 10:19:25 +0700

    handle api youtube on submit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
+import { Grid } from '@material-ui/core';
+import { SearchBar, VideoDetail, VideoList } from './components';
+import youtube from './api/youtube';
 
 export class App extends Component {
+  handleSubmit = async (searchTerm) => {
+    const response = await youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: 'AIzaSyCStNQBcibyGypy17t23L2RZNnflvHnfNU',
+        q: searchTerm,
+      },
+    });
+    console.log(response);
+  };
   render() {
-    return <div>Youtube Clone App</div>;
+    return (
+      <Grid justify='center' container spacing={10}>
+        <Grid item xs={12}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              <SearchBar onFormSubmit={this.handleSubmit} />
+            </Grid>
+            <Grid item xs={8}>
+              <VideoDetail />
+            </Grid>
+            <Grid item xs={4}>
+              {/* <VideoList></VideoList> */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
